ba5c2afb212bba16cc27cdf79cfd55cae7b8318d
unknown <jkvapil6@gmail.com>
2020-02-18 22:08:28 +0100

    User-definable indicators

---
+++ components/IndicatorAddForm.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react"
+import { Picker, StyleSheet, View, Text, Button, TextInput } from "proton-native"
+
+const Indicators = require("../js/Indicators")
+
+export default class IndicatorAddForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      indicatorName: "ma100",
+      timeframe: 100,
+      indicator: "Moving Average"
+    }
+  }
+
+
+  render() {
+    const styles = StyleSheet.create({
+      title: { fontWeight: 'bold' },
+      container: { backgroundColor: "#eeffff" }
+    })
+    
+    return (
+      <View>
+        <Text style={{ fontWeight: 'bold' }}> Define your indicators: </Text> 
+        <Text> name: </Text> 
+        <TextInput
+            style={{ borderWidth: 1, width: '200px' }}  
+            value={this.state.indicatorName}
+            onChangeText={(e) => {
+              this.setState({indicatorName: e})
+          }}
+        />
+
+        <Text> timeframe: </Text> 
+        <TextInput
+            style={{ borderWidth: 1, width: '200px' }}  
+            value={ this.state.timeframe.toString(10) }
+            onChangeText={(e) => {
+              const n = Number.parseInt(e)
+              if (!Number.isNaN(n)) this.setState({timeframe: n}) 
+            }}
+        />
+
+        <Text> indicator: </Text> 
+        <Picker
+          selectedValue={this.state.indicator}
+          style={{height: 25, width: 100}}
+          onValueChange={(itemValue, itemIndex) => this.setState({indicator: itemValue})}
+        >
+        <Picker.Item label="Moving Average" value="Moving Average" />
+        <Picker.Item label="MACD" value="MACD" />
+      </Picker>
+        
+        <Button 
+          style={{ width: '200px' }} 
+          title="Add" 
+          onPress={ () => { 
+            console.log(this.state.indicator, this.state.timeframe) 
+            
+            let f
+
+            switch(this.state.indicator) {
+              case "Moving Average" : f = Indicators.average(this.state.timeframe)
+                break
+            }
+
+            this.props.addIndicator(this.state.indicatorName, this.state.timeframe, f)
+
+          }} />
+      </View>
+    );
+  }
+}
